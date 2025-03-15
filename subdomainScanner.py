f41aba81ed98d36bac114bb7f05c7346cfc62e75
moGAMGOMA <127643668+Gamgoma00@users.noreply.github.com>
2025-03-15 23:48:22 +0200

    Update subdomainScanner.py

--- subdomainScanner.py
+++ subdomainScanner.py
@@ -1,14 +1,12 @@
 import requests
 import sys
 
-# التأكد من وجود الدومين كوسيط
 if len(sys.argv) < 2:
     print("Usage: python script.py <domain>")
     sys.exit(1)    
 
 domain = sys.argv[1]
 
-# فتح ملف subdomains.txt بطريقة آمنة باستخدام with
 with open("subdomains.txt", "r") as file:
     subdoms = file.read().splitlines()
 
@@ -17,11 +15,10 @@ for sub in subdoms:
     
     try:
         response = requests.get(sub_domains)
-        # لو الصفحة مش موجودة (404) ننتقل للعنصر التالي
         if response.status_code == 404:
             continue
     except requests.ConnectionError:
-        continue  # لو حصل خطأ في الاتصال، ننتقل للعنصر التالي
+        continue
     except Exception as e:
         print(f"Error accessing {sub_domains}: {e}")
         continue
